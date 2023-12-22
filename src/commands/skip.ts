6d8659e33d26ae786f64778238ac3be571c17617
Abbe Azale <000394548@student.vcc.ca>
2023-12-22 13:49:31 -0800

    skip function

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -1,7 +1,10 @@
 import {CommandInteraction, SlashCommandBuilder, GuildMember, Client} from 'discord.js';
 const { Track} = require('discord-player');
 import {client} from '../index';
-const { useMainPlayer } = require('discord-player');
+const {  Player } = require('discord-player');
+import {useMainPlayer} from 'discord-player';
+import { useTimeline, useQueue } from 'discord-player';
+
 
 
 
@@ -10,6 +13,23 @@ export const data = new SlashCommandBuilder()
     .setDescription('Skips the current song')
 
 export async function execute( interaction: CommandInteraction){
+
+    await interaction.deferReply();
+    const queue = useQueue(interaction.guildId!);
+
+    if (!queue) {
+        return interaction.followUp('Nothing is currently playing!');
+    }   
+
+   try {
+        queue.node.skip();
+        return interaction.followUp('Skipped the current song');
+   } catch (e) {
+        console.log('error', e);
+        return interaction.followUp(`Something went wrong: ${e}`);
+   }
+ 
+
     
 }
 
