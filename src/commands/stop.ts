87866d356248710ae304640251b0d7d94656b47c
Abbe Azale <000394548@student.vcc.ca>
2023-12-22 15:59:46 -0800

    stop function

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,5 +1,25 @@
 import {SlashCommandBuilder, CommandInteraction} from 'discord.js';
+import {useQueue, useTimeline} from 'discord-player';
 
 export const data = new SlashCommandBuilder()
     .setName('stop')
     .setDescription('Stops the current song');
+
+export async function execute(interaction: CommandInteraction){
+
+    const timeLine = useTimeline(interaction.guildId!);
+    await interaction.deferReply();
+    const queue = useQueue(interaction.guildId!);
+
+    if(!timeLine){
+        return interaction.followUp('Nothing is currently playing');
+    }
+
+    try {
+        queue?.node.stop();
+        return interaction.followUp('Stopped the current song');
+    } catch(e) {
+        console.log('error', e);
+        return interaction.followUp(`Something went wrong: ${e}`);
+    }
+}
